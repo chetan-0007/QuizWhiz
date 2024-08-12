8481f2178ea5efecc54097592ae7aa7d8f1c3a9e
Chetan <97439606+chetan-0007@users.noreply.github.com>
2024-08-13 01:21:50 +0530

    Add files via upload

---
+++ backend/Models/Participant.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace QuizAPI.Models
+{
+    public class Participant
+    {
+        [Key]
+        public int ParticipantID { get; set; }
+        [Column(TypeName = "nvarchar(50)")]
+        public string Name { get; set; }
+
+        [Column(TypeName = "nvarchar(50)")]
+        public string Email { get; set; }
+        public int Score { get; set; }
+        public int TimeTaken { get; set; }
+
+    }
+}
